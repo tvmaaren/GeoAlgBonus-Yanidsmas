7cdea714d7ea7d77b7dbe754a0604dac42deb17e
Thomas van Maaren <thomas.v.maaren@outlook.com>
2025-01-23 08:00:28 +0100

    graham_scan should work on all degenerate cases

--- src/graham_scan.rs
+++ src/graham_scan.rs
@@ -1,6 +1,6 @@
 pub mod graham_scan{
 
-use partial_min_max::min;
+use partial_min_max::{min,max};
 use std::cmp::Ordering::{Equal,Less,Greater};
 
 
@@ -14,7 +14,7 @@ enum VSide {
 
 
 fn is_vside(vside : VSide, (x1,y1) : (f64,f64), (x2,y2) : (f64,f64), (x3,y3) : (f64,f64)) -> bool {
-    let y = if x1==x3 {min(y1,y3)} else {(y3-y1)/(x3-x1)*x2+y1};
+    let y = if x1==x3 {if vside==VSide::Top {max(y1,y3)} else {min(y1,y3)}} else {(y3-y1)/(x3-x1)*(x2-x1)+y1};
     if vside == VSide::Top {
         return y>=y2;
     }else{
@@ -26,7 +26,7 @@ fn is_vside(vside : VSide, (x1,y1) : (f64,f64), (x2,y2) : (f64,f64), (x3,y3) : (
 fn convex_hull_vside (vside : VSide, sorted_points : &[(f64,f64)]) -> Vec<(f64,f64)> {
     let mut convex_hull: Vec<(f64,f64)> = Vec::new();
     for point in sorted_points {
-        if convex_hull.len()<=2{
+        if convex_hull.len()<2{
             convex_hull.push(*point);
             continue
         }
@@ -35,11 +35,11 @@ fn convex_hull_vside (vside : VSide, sorted_points : &[(f64,f64)]) -> Vec<(f64,f
             if is_vside(vside,convex_hull[i-1],convex_hull[i],*point) {
                 convex_hull.pop();
             }else{
-                convex_hull.push(*point);
                 break;
             }
             i-=1;
         }
+        convex_hull.push(*point);
     }
     return convex_hull;
 }
@@ -78,8 +78,8 @@ fn union_sorted<T: PartialOrd+Copy>(a:Vec<T>,b:Vec<T>) -> Vec<T>{
 
 
 pub fn graham_scan(mut points : Vec<(f64,f64)>) -> Vec<(f64,f64)> {
-    points.sort_by(|(x1,_), (x2,_)| (*x1 as i64).cmp(&(*x2 as i64)));
-    let convex_hull_top = convex_hull_vside(VSide::Top, &points);
+    points.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Equal));
+    let convex_hull_top= convex_hull_vside(VSide::Top, &points);
     let convex_hull_bottem = convex_hull_vside(VSide::Bottem, &points);
     let convex_hull = union_sorted(convex_hull_top,convex_hull_bottem);
     //println!("All the points {:?}",points);
